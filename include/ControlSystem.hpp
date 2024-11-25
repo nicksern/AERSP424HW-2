9e7a82c28c3ee752af533b8e1cc22ac2ade2a41f
nicksern <nms6249@psu.edu>
2024-11-25 16:45:46 -0500

    added more comments to control system and sensor factory header files

--- include/ControlSystem.hpp
+++ include/ControlSystem.hpp
@@ -4,16 +4,22 @@
 // AerospaceControlSystem class
 class AerospaceControlSystem {
 private:
+    // Setting up sensor pointer via a vector
     vector<unique_ptr<Sensor>> sensors;
 public:
+    // Function to add sensor using its pointer
     void addSensor(unique_ptr<Sensor> sensor) {
         sensors.push_back(move(sensor));
     }
     
+    // Function to gather and process sensor data
     void monitorAndAdjust() {
         for (auto& sensor : sensors) {
+            // Set up for data collection
             sensor->gatherData();
+            // Set up for data processing
             sensor->processData();
+            // Output statement showing the system is adjusting based on sensor data
             cout << "Adjusting controls based on sensor data." << endl;
         }
     }
--- include/SensorFactory.hpp
+++ include/SensorFactory.hpp
@@ -1,16 +1,19 @@
 #include "Sensor.hpp"
 
-// SensorFactory class
+// Sensor factory class
 class SensorFactory {
 public:
     static unique_ptr<Sensor> createSensor(const string& sensorType) {
-        // 
+        // Setting pointer for altimeter sensor string
         if (sensorType == "Altimeter") {
             return make_unique<Altimeter>();
+        // Setting pointer for pitot tube sensor string
         } else if (sensorType == "PitotTube") {
             return make_unique<PitotTube>();
-        } else if (sensorType == "AngleOfAttackProbe") {
+        // Setting pointer for gyroscope sensor string
+        } else if (sensorType == "Gyroscope") {
             return make_unique<Gyroscope>();
+        // If not sensor string is provided return a null pointer
         } else {
             return nullptr;
         }
